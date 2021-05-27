fe4a1a96997297289e80003976fdd1302139d01e
galdinodeivid <galdinodeivid@gmail.com>
2021-05-27 16:34:09 -0300

    Início do projeto.

--- JantinhaCalc/src/principal/Principal.java
+++ JantinhaCalc/src/principal/Principal.java
@@ -1,47 +1,33 @@
 package principal;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) throws IOException {
 		
-		//Exibir o logo e comandos do programa
-		Path arquivoLogo = Paths.get("logo.txt");
-		Scanner logo = new Scanner(arquivoLogo);
-		String line = "";
-		
-		while (logo.hasNextLine()) {
-		    line += " " + logo.nextLine() + "\n";
-		}
-		
-		System.out.println(line);
-		logo.close();
-		
-		//Comandos
 		Scanner input = new Scanner(System.in);
+		double[] vetorCustos = {3,4,2,3};
+		int[][] numsPorcoes = {{2,1,1,1},{1,2,1,1},{2,2,0,3}};
 		String comando;
+	
+		System.out.println(Utils.printLogo());
 		
 		do {
-			System.out.print("administrador@jantinhacalc$ ");
+			System.out.print("administrador@jantinhacalc~$ ");
 			comando = input.nextLine();
 			
 			switch (comando) {
-			case "avp":
-				System.out.println(":)");
+			case "exit":
+				System.out.println("Saindo do programa...");
 				break;
-			case "mcp":
-				System.out.println(";-;");
-				break;
-			case "cpp":
-				System.out.println("hehehe");
-				break;
+			default:
+				System.out.println("[ERRO] Comando não encontrado!");
 			}
 			
-		} while (true);
+		} while (!comando.equals("exit"));
+		
 		
 	}
 	
--- JantinhaCalc/src/principal/Utils.java
+++ JantinhaCalc/src/principal/Utils.java
@@ -1,5 +1,25 @@
 package principal;
 
-public class Utils {
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
+public class Utils {
+	
+	public static String printLogo() throws IOException {
+		
+		Path arquivoLogo = Paths.get("logo.txt");
+		Scanner logo = new Scanner(arquivoLogo);
+		String line = "";
+		
+		while (logo.hasNextLine()) {
+		    line += " " + logo.nextLine() + "\n";
+		}
+		
+		logo.close();
+		return line;
+	}
+	
+	
 }
